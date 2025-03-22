70fa976e2662366afef823500c1763d8459e7293
renkunx <rk-tingyu@outlook.com>
2025-03-22 10:18:22 +0800

    feat: 增加Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { BottomNav } from "@/components/bottom-nav"
 import { Logo } from "@/components/logo"
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from "@/components/auth/auth-provider"
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -33,6 +33,7 @@ export default function RootLayout({
             </div>
           </header>
           <main className="container mx-auto px-4 py-6 pb-24">{children}</main>
+          <Analytics />
           <BottomNav />
           <Toaster />
         </AuthProvider>
